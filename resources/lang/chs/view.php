fa127ec256a00b388a03d693f9a3668084741277
shangjun <shangjun.jiang@patpat.com>
2020-03-26 18:36:17 +0800

    更新

---
+++ resources/lang/chs/view.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'email' => '邮箱',
+    'password' => '密码',
+    'remember_me' => '记住我',
+    'forgot_password' => '忘记密码',
+    'login' => '登录',
+    'register' => '注册',
+
+
+];
